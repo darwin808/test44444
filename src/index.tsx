ea848c357c7123699a569eeba1c224cf90422569
darwin808 <darwin.apolinario12@gmail.com>
2022-04-11 10:57:09 +0800

    initialize cdn for zesty explorer

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ZestyExplorer } from "./components/ZestyExplorer";
+import { canUseDOM } from "./utils/index";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const main = () => {
+  if (!canUseDOM()) {
+    return null;
+  }
+  document.body.innerHTML += '<div id="zesty-explorer"></div>';
+  return ReactDOM.render(
+    <ZestyExplorer />,
+    document.getElementById("zesty-explorer")
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+main();
